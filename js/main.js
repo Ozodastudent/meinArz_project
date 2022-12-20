4ad4afe78afd482efbdecf6003c2d265afda61e7
Ozoda Shakhobiddinova <2110074@newuzbekistanuniversity.uz>
2022-12-21 00:49:44 +0500

    I have added changes

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@ $(".opinion_cards_list").slick({
   autoplay: true,
   autoplaySpeed: 1000,
 });
+// nav open
+
+const elHeader = document.querySelector(".header");
+const openToggleBtn = document.querySelector(".toggle_open_btn");
+
+openToggleBtn.addEventListener("click", function () {
+  document.body.classList.toggle("body-fixed");
+  elHeader.classList.toggle("header-open");
+});
---
+++ js/register.js
@@ -0,0 +1,32 @@
+const elRegisterForm = document.querySelector(".sign_main_left_form");
+const elRegisterFormName = document.querySelector(".user_name_input");
+const elRegisterFormEmail = document.querySelector(".user_email_input");
+
+async function registerUsers() {
+  try {
+    const res = await fetch("http://localhost:5000/user/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_name: elRegisterFormName.value,
+        email: elRegisterFormEmail.value,
+        password: elRegisterFormPassword.value,
+      }),
+    });
+    const data = await res.json();
+    console.log(data);
+    if (data.token) {
+      console.log(data);
+      localStorage.setItem("register-token", data.token);
+      window.location.pathname = "/index.html";
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+elRegisterForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  registerUsers();
+});
